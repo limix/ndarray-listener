da83491ee0380c93d72a86014eb52cb0f6eb8679
Danilo Horta <danilo.horta@gmail.com>
2017-08-28 16:50:30 +0100

    Listener for setting after copy

--- ndarray_listener/ndarray_listener.py
+++ ndarray_listener/ndarray_listener.py
@@ -4,7 +4,12 @@ from numpy import asarray, ndarray
 class ndarray_listener(ndarray):
     def __new__(cls, input_array):
         obj = asarray(input_array).view(cls)
-        obj._listeners = []
+
+        if hasattr(input_array, '_listeners'):
+            obj._listeners = input_array._listeners
+        else:
+            obj._listeners = []
+
         return obj
 
     def __array_finalize__(self, obj):
@@ -18,18 +23,26 @@ class ndarray_listener(ndarray):
 
     def __setattr__(self, *args, **kwargs):
         super(ndarray_listener, self).__setattr__(*args, **kwargs)
+        if len(args) > 0 and args[0] == '_listeners':
+            return
         self.__notify()
 
     def __getitem__(self, *args, **kwargs):
         v = super(ndarray_listener, self).__getitem__(*args, **kwargs)
-        return ndarray_listener(v)
+        if isinstance(v, ndarray_listener):
+            return v
+
+        v = ndarray_listener(v)
+        for l in self._listeners:
+            v.talk_to(l)
+        return v
 
     def talk_to(self, me):
         self._listeners.append(me)
 
     def __notify(self):
         for l in self._listeners:
-            l(super(ndarray_listener, self))
+            l(asarray(self))
 
     def itemset(self, *args, **kwargs):
         super(ndarray_listener, self).itemset(*args, **kwargs)
