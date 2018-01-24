cdd90fe45e2255ad8901e2eded5b8a0e6cec23de
Danilo Horta <danilo.horta@gmail.com>
2018-01-24 21:42:37 +0000

    Numpy 1.4 printing bug

--- ndarray_listener/ndarray_listener.py
+++ ndarray_listener/ndarray_listener.py
@@ -56,6 +56,9 @@ class ndarray_listener(np.ndarray):
 
         >>> from ndarray_listener import ndarray_listener
         >>> from numpy import atleast_1d
+        >>> from numpy import set_printoptions
+        >>>
+        >>> set_printoptions(precision=2, suppress=True)
         >>>
         >>> vector = ndarray_listener([-0.5, 0.1])
         >>>
@@ -65,7 +68,7 @@ class ndarray_listener(np.ndarray):
         >>> vector.talk_to(you0)
         >>>
         >>> vector[0] = 0.0
-        First guy called with [0.  0.1]
+        First guy called with [0. 0.1]
         >>> vector[:] = 1.0
         First guy called with [1. 1.]
         >>>
@@ -80,8 +83,8 @@ class ndarray_listener(np.ndarray):
         >>>
         >>> v1.talk_to(you1)
         >>> vector[-1] = 9.9
-        First guy called with [1.  9.9]
-        Second guy called with [1.  9.9]
+        First guy called with [1. 9.9]
+        Second guy called with [1. 9.9]
     """
 
     def __new__(cls, input_array):
