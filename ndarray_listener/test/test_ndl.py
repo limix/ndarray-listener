900fe60e8f2fc9e07bfc30c7a8f116682b98d40d
Danilo Horta <danilo.horta@gmail.com>
2018-01-25 12:26:00 +0000

    scalar

--- ndarray_listener/test/test_ndl.py
+++ ndarray_listener/test/test_ndl.py
@@ -1,12 +1,12 @@
-from ndarray_listener import ndarray_listener
-from numpy import array
-from numpy.testing import assert_, assert_array_almost_equal
+import numpy as np
+from ndarray_listener import float64, ndl
+from numpy.testing import assert_, assert_array_almost_equal, assert_equal
 
 
 def test_operations():
-    a = array([-0.5, 0.1, 1.1])
-    b = ndarray_listener(a)
-    c = ndarray_listener(array([-0.5, 0.1, 1.1]))
+    a = np.array([-0.5, 0.1, 1.1])
+    b = ndl(a)
+    c = ndl(np.array([-0.5, 0.1, 1.1]))
 
     assert_array_almost_equal(a - b, [0, 0, 0])
     assert_array_almost_equal(a, b)
@@ -15,15 +15,15 @@ def test_operations():
 
 
 def test_itemset():
-    a = ndarray_listener(-0.5)
+    a = ndl(-0.5)
     a.itemset(1.0)
     assert_array_almost_equal(a, [1])
 
 
 def test_notification():
-    a = array([-0.5, 0.1, 1.1])
-    b = ndarray_listener(a)
-    c = ndarray_listener(array([-0.5, 0.1, 1.1]))
+    a = np.array([-0.5, 0.1, 1.1])
+    b = ndl(a)
+    c = ndl(np.array([-0.5, 0.1, 1.1]))
 
     class Watcher(object):
         def __init__(self):
@@ -56,23 +56,28 @@ def test_notification():
 
 
 def test_iterator():
-    a = ndarray_listener([-0.5, 0.1, 1.1])
-    assert_(isinstance(next(iter(a)), ndarray_listener))
+    a = ndl([-0.5, 0.1, 1.1])
+    assert_(isinstance(next(iter(a)), float64))
 
 
 def test_printing(capsys):
-    a = ndarray_listener(array([-0.5, 0.1, 1.1]))
+    a = ndl(np.array([-0.5, 0.1, 1.1]))
 
     print(a)
     out, _ = capsys.readouterr()
-    assert_(out == "[-0.5  0.1  1.1]\n")
+    assert_equal(out, "[-0.5  0.1  1.1]\n")
     print([a])
     out, _ = capsys.readouterr()
-    assert_(out == "[ndarray_listener([-0.5,  0.1,  1.1])]\n")
+    assert_equal(out, "[ndl([-0.5,  0.1,  1.1])]\n")
+
+    a = ndl(np.float64(1.0))
+    print(a)
+    out, _ = capsys.readouterr()
+    assert_equal(out, "1.0\n")
 
 
 def test_scalar_copy_listener():
-    a = ndarray_listener(-0.5)
+    a = ndl(-0.5)
 
     class Watcher(object):
         def __init__(self):
@@ -86,7 +91,7 @@ def test_scalar_copy_listener():
 
     a.talk_to(you0)
 
-    b = ndarray_listener(a)
+    b = ndl(a)
     assert_(not you0.called_me)
 
     b.itemset(1.0)
@@ -103,7 +108,7 @@ def test_scalar_copy_listener():
 
 
 def test_array_copy_listener():
-    a = ndarray_listener([-0.5, 0.1, 1.1])
+    a = ndl([-0.5, 0.1, 1.1])
 
     class Watcher(object):
         def __init__(self):
@@ -117,7 +122,7 @@ def test_array_copy_listener():
 
     a.talk_to(you0)
 
-    b = ndarray_listener(a)
+    b = ndl(a)
     assert_(not you0.called_me)
 
     b[0] = 1.0
