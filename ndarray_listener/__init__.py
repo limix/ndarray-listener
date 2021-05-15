e1d9030ebddff8c21d1993da5ab76bc69bd45023
Danilo Horta <danilo.horta@pm.me>
2021-05-16 00:34:42 +0100

    2.0.1

--- ndarray_listener/__init__.py
+++ ndarray_listener/__init__.py
@@ -1,9 +1,7 @@
-from __future__ import absolute_import as _absolute_import
-
 from ._ndl import float64, ndl
 from ._testit import test
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 
 ndarray_listener = ndl
 
